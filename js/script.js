04b2ac3a9e9ebbe128f68c7cf7e02458588bcb31
stephanieparra <stephanieparrastudio@gmail.com>
2024-01-30 18:34:15 -0800

    Added global variables

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Global Variables//
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+//Tester word//
+const word = "magnolia";
