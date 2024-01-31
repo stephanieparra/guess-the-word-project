d0597e49ae20c5eaf20d841640be5c9db64c1af4
stephanieparra <stephanieparrastudio@gmail.com>
2024-01-30 18:53:39 -0800

    Added function for placeholder and button event listener

--- js/script.js
+++ js/script.js
@@ -7,5 +7,25 @@ const remainingGuesses = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
+
 //Tester word//
 const word = "magnolia";
+
+//Display placeholder symbols for the tester word's letters//
+const placeholder = function (word) {
+  const placeholderLetters = [];
+  for (const letter of word) {
+    console.log(letter);
+    placeholderLetters.push("●");
+  }
+  wordInProgress.innerText = placeholderLetters.join("");
+};
+placeholder(word);
+
+//Event listener for guess button//
+guessButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const personsGuess = letterInput.value;
+  console.log(personsGuess);
+  letterInput.value = "";
+});
